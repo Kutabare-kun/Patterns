ddb6e8033d84ae83ce6db2c149e4117cdd030205
Alexander Zoshchuk <78682049+Kutabare-kun@users.noreply.github.com>
2024-10-05 12:05:11 +0300

    Added example combine Facade and Strategy Pattern

---
+++ Pattern/FacadeAndStrategy/FacadeAndStrategy.cpp
@@ -0,0 +1,41 @@
+﻿#include "FacadeAndStrategy.h"
+
+#include <iostream>
+
+void SwordAttack::ExecuteAttack(const std::string& Enemy)
+{
+    std::cout << "Sword attack to " << Enemy << '\n';
+}
+
+void MagicAttack::ExecuteAttack(const std::string& Enemy)
+{
+    std::cout << "Magic attack to " << Enemy << '\n';
+}
+
+void RangeAttack::ExecuteAttack(const std::string& Enemy)
+{
+    std::cout << "Range attack to " << Enemy << '\n';
+}
+
+CharacterFacade::CharacterFacade(std::string&& CharacterName)
+    : Name(std::move(CharacterName))
+{
+}
+
+void CharacterFacade::SetAttackStrategy(std::unique_ptr<AttackStrategy>&& NewAttackStrategy)
+{
+    MyAttackStrategy = std::move(NewAttackStrategy);
+}
+
+void CharacterFacade::PerformAttack(const std::string& Enemy) const
+{
+    if (MyAttackStrategy)
+    {
+        std::cout << Name << " prepare to attack " << Enemy << '\n';
+        MyAttackStrategy->ExecuteAttack(Enemy);
+    }
+    else
+    {
+        std::cout << Name << " has no attack strategy\n";
+    }
+}
---
+++ Pattern/FacadeAndStrategy/FacadeAndStrategy.h
@@ -0,0 +1,42 @@
+﻿#pragma once
+#include <memory>
+#include <string>
+
+class AttackStrategy
+{
+public:
+    virtual ~AttackStrategy() = default;
+    virtual void ExecuteAttack(const std::string& Enemy) = 0;
+};
+
+class SwordAttack : public AttackStrategy
+{
+public:
+    void ExecuteAttack(const std::string& Enemy) override;
+};
+
+class MagicAttack : public AttackStrategy
+{
+public:
+    void ExecuteAttack(const std::string& Enemy) override;
+};
+
+class RangeAttack : public AttackStrategy
+{
+public:
+    void ExecuteAttack(const std::string& Enemy) override;
+};
+
+class CharacterFacade
+{
+public:
+    CharacterFacade(std::string&& CharacterName);
+
+    void SetAttackStrategy(std::unique_ptr<AttackStrategy>&& NewAttackStrategy);
+
+    void PerformAttack(const std::string& Enemy) const;
+
+private:
+    std::string Name;
+    std::unique_ptr<AttackStrategy> MyAttackStrategy;
+};
