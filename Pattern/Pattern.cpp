121bd700b9e4460e27f3b971af1123551dc3d763
Alexander Zoshchuk <78682049+Kutabare-kun@users.noreply.github.com>
2024-10-05 11:02:59 +0300

    Added Strategy Pattern

--- Pattern/Pattern.cpp
+++ Pattern/Pattern.cpp
@@ -1,4 +1,8 @@
+#include <iostream>
+#include <format>
+
 #include "Observer/Observer.h"
+#include "Strategy/Strategy.h"
 
 class Manager
 {
@@ -30,6 +34,29 @@ public:
         // 1 observers
         MySubject->Calculation(std::make_tuple(5, 6));
     }
+
+    void StrategyExample()
+    {
+        std::unique_ptr<Strategy> MyStrategy = std::make_unique<MultiplyDamage>(2.0f);
+        Context MyContext(std::move(MyStrategy));
+
+        std::cout << std::format("MultiplyDamage: {}\n", MyContext.ExecutionDamage(10.0f));
+
+        MyStrategy = std::make_unique<DivisionDamage>(2.0f);
+        MyContext.SetStrategy(std::move(MyStrategy));
+
+        std::cout << std::format("DivisionDamage: {}\n", MyContext.ExecutionDamage(10.0f));
+
+        MyStrategy = std::make_unique<AddDamage>(2.0f);
+        MyContext.SetStrategy(std::move(MyStrategy));
+
+        std::cout << std::format("AddDamage: {}\n", MyContext.ExecutionDamage(10.0f));
+
+        MyStrategy = std::make_unique<SubtractionDamage>(2.0f);
+        MyContext.SetStrategy(std::move(MyStrategy));
+
+        std::cout << std::format("SubtractionDamage: {}\n", MyContext.ExecutionDamage(10.0f));
+    }
 };
 
 
@@ -37,7 +64,7 @@ int main(int argc, char* argv[])
 {
     Manager JustManager;
 
-    JustManager.ObserverExample();
+    JustManager.StrategyExample();
 
     return 0;
 }
---
+++ Pattern/Strategy/Strategy.h
@@ -0,0 +1,96 @@
+﻿#pragma once
+#include <memory>
+
+// See example at https://refactoring.guru/design-patterns/strategy/cpp/example
+class Strategy
+{
+public:
+    // Execute the strategy
+    // @param Damage: Damage to execute
+    virtual float ExecutionDamage(float Damage) = 0;
+};
+
+class MultiplyDamage : public Strategy
+{
+public:
+    explicit MultiplyDamage(float Multiplier) : Multiplier(Multiplier)
+    {
+    }
+
+    float ExecutionDamage(float Damage) override
+    {
+        return Damage * Multiplier;
+    }
+
+private:
+    float Multiplier;
+};
+
+class DivisionDamage : public Strategy
+{
+public:
+    explicit DivisionDamage(float Divider) : Divider(Divider == 0.0f ? 1.0f : Divider)
+    {
+    }
+
+    float ExecutionDamage(float Damage) override
+    {
+        return Damage / Divider;
+    }
+
+private:
+    float Divider;
+};
+
+class AddDamage : public Strategy
+{
+public:
+    explicit AddDamage(float Adder) : Adder(Adder)
+    {
+    }
+
+    float ExecutionDamage(float Damage) override
+    {
+        return Damage + Adder;
+    }
+
+private:
+    float Adder;
+};
+
+class SubtractionDamage : public Strategy
+{
+public:
+    explicit SubtractionDamage(float Subtractor) : Subtractor(Subtractor)
+    {
+    }
+
+    float ExecutionDamage(float Damage) override
+    {
+        return Damage - Subtractor;
+    }
+
+private:
+    float Subtractor;
+};
+
+class Context
+{
+public:
+    explicit Context(std::unique_ptr<Strategy>&& NewStrategy) : MyStrategy(std::move(NewStrategy))
+    {
+    }
+
+    void SetStrategy(std::unique_ptr<Strategy>&& NewStrategy)
+    {
+        MyStrategy = std::move(NewStrategy);
+    }
+
+    float ExecutionDamage(float Damage)
+    {
+        return MyStrategy->ExecutionDamage(Damage);
+    }
+
+private:
+    std::unique_ptr<Strategy> MyStrategy;
+};
